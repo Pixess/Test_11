59eecc8a6b6a4e6319dca0b9882cfa155ee86707
Artem <Kremsartem@gmail.com>
2019-12-01 16:01:01 +0200

    Initial Commit

---
+++ test_11/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test_11
+//
+//  Created by Artem Kremenchutsky on 01.12.2019.
+//  Copyright © 2019 Artem Kremenchutsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
